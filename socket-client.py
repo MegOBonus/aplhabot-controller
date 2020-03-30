c1e9fa7de5061a1e9a6a9ba59e935c92bcb33cb3
Vitalii <megobonus666@gmail.com>
2020-03-30 22:18:04 +0300

    Add socket client for control

---
+++ socket-client.py
@@ -0,0 +1,49 @@
+import socket
+from pynput import keyboard
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket successfully created")
+
+port = 65432
+host_ip = '192.168.0.106'
+
+# connecting to the server
+s.connect((host_ip, port))
+
+print("connected to pi on port == ", host_ip)
+print("Windows client is ready!\r\n")
+
+
+def handle_click(key):
+    if key == keyboard.Key.up:
+        s.send(b'forward')
+    if key == keyboard.Key.down:
+        s.send(b'backward')
+    if key == keyboard.Key.left:
+        s.send(b'left')
+    if key == keyboard.Key.right:
+        s.send(b'right')
+    if key == '+':
+        s.send(b'speed up')
+    if key == '-':
+        s.send(b'speed down')
+
+
+def on_press(key):
+    try:
+        handle_click(key.char)
+    except AttributeError:
+        handle_click(key)
+
+
+def on_release(key):
+    s.send(b'stop')
+    if key == keyboard.Key.esc:
+        return False
+
+
+while True:
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
